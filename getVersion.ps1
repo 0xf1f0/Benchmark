4d8702f08a8452ef27764155d0d22bdfadf5f0e7
Busiedblowfish <oobey1@aol.com>
2016-07-11 14:03:16 -0500

    Gets OS and PS versions

---
+++ getVersion.ps1
@@ -0,0 +1,18 @@
+﻿function getPSversion
+{
+    psVersion = Get-Host | Select-Object Version
+    return psVersion
+}
+
+
+function getWinversion
+{
+    try
+    {
+        $osVersion = Get-CimInstance Win32_OperatingSystem | Select-Object Caption
+    }
+    catch [System.Exception]
+    {
+       
+    }
+}
